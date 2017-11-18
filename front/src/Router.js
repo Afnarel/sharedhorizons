33e7c750832dad7d9d0c20c1f708dff763a0789d
Anthony Saraïs <anthony.sarais@greencom-networks.com>
2017-11-18 11:52:04 +0100

    added VR button and VR route

--- front/src/Router.js
+++ front/src/Router.js
@@ -11,6 +11,7 @@ import History from './views/history/History';
 import Channel from './views/channel/Channel';
 import Video from './views/video/Video';
 import NotFound from './views/notFound/NotFound';
+import VR from './views/vr/VR';
 
 
 const routes = () => {
@@ -26,6 +27,7 @@ const routes = () => {
                         <Route exact path="/history" component={History}/>
                         <Route exact path="/channel/:name" component={Channel}/>
                         <Route exact path="/video/:videoId" component={Video}/>
+                        <Route exact path="/VR" component={VR}/>
                         <Route path='*' component={NotFound}/>
                     </Switch>
                 </div>
--- front/src/views/vr/VR.js
+++ front/src/views/vr/VR.js
@@ -3,12 +3,12 @@ import 'aframe';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 
-//import './VR.scss';
+import './VR.scss';
 
 class VR extends React.Component {
   render () {
     return (
-      <Scene>
+      <Scene className="VrRoot">
         <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
         <Entity light={{type: 'point'}}/>
         <Entity text={{value: 'Shared Horizons'}}/>
