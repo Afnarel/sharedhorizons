81596fa85c651251dad4df8ed8018bf24e7f9b7d
brucelane <bruce@batchass.fr>
2017-11-18 11:40:27 +0100

    aframe-react added

---
+++ front/src/views/vr/VR.js
@@ -0,0 +1,19 @@
+import 'aframe';
+// import 'aframe-particle-system-component'; <Entity particle-system={{preset: 'snow'}}/>
+import {Entity, Scene} from 'aframe-react';
+import React from 'react';
+
+//import './VR.scss';
+
+class VR extends React.Component {
+  render () {
+    return (
+      <Scene>
+        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+        <Entity light={{type: 'point'}}/>
+        <Entity text={{value: 'Shared Horizons'}}/>
+      </Scene>
+    );
+  }
+}
+export default VR;
